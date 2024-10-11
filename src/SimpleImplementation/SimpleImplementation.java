b7013e386b91822dc68509395560170a65cbf23c
DavramJavier <Harukasensei25@gmail.com>
2024-10-11 18:42:35 +0800

    Added Output and Process

--- src/SimpleImplementation/SimpleImplementation.java
+++ src/SimpleImplementation/SimpleImplementation.java
@@ -15,7 +15,12 @@ public class SimpleImplementation {
     
     //Array 
     int[] num = {1, 2, 3, 4, 5};    
-   
+    //Output and Process by calling its index to print values
+    System.out.println(num[0]);
+    System.out.println(num[1]);
+    System.out.println(num[2]);
+    System.out.println(num[3]);
+    System.out.println(num[4]);
     }
     
 
