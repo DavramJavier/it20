7198649c01a9e4e021b31357406c548107cf73ef
DavramJavier <Harukasensei25@gmail.com>
2024-10-11 18:33:37 +0800

    Added Multidimensional Array

---
+++ src/MultidimensionalArray/MultidimensionalArray.java
@@ -0,0 +1,25 @@
+    /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package MultidimensionalArray;
+
+/**
+ *
+ * @author HP
+ */
+public class MultidimensionalArray {
+     public static void main(String[] args) {
+         
+         //2D Array of Program and Year
+         String[][] programAndyear ={
+             {"BSIT", "First year"},
+             {"BSBA", "Second year"},   
+             {"BSED", "Third year"}
+         };
+         
+     }
+
+}
