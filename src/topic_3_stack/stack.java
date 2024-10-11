3336a72a607afd2389459ba81cc65e3bf5f7ddd5
DavramJavier <Harukasensei25@gmail.com>
2024-10-12 00:35:30 +0800

    Added Push

--- src/topic_3_stack/stack.java
+++ src/topic_3_stack/stack.java
@@ -33,4 +33,13 @@ public class stack {
         return top + 1;
     }
     
+     public void push(int value) {
+        if (isFull()) {
+            System.out.println("Stack is full. Cannot push " + value);
+            return;
+        }
+        stackArray[++top] = value;
+    }
+     
+    
 }
