4e9d5baed7c2527cbd0ddc8f0aecc7952bb4a738
DavramJavier <Harukasensei25@gmail.com>
2024-10-12 00:31:33 +0800

    Added Stack

---
+++ src/topic_3_stack/stack.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package topic_3_stack;
+
+/**
+ *
+ * @author HP
+ */
+public class stack {
+    private int maxSize;
+    private int[] stackArray;
+    private int top;
+    
+    public stack(int size) {
+        this.maxSize = size;
+        this.stackArray = new int[maxSize];
+        this.top = -1; 
+    }
+    
+    
+    
+}
