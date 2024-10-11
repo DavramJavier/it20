5a462a25f44079aa6dd2b2d89fd9290348026b30
DavramJavier <Harukasensei25@gmail.com>
2024-10-11 22:51:58 +0800

    Added Method

---
+++ src/topic_2_linkedlist_int/LinkedList.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package topic_2_linkedlist_int;
+
+/**
+ *
+ * @author HP
+ */
+public class LinkedList {
+     
+    private Node head; 
+    
+        // Method to add a new node in the list
+        public void add(int data) {
+            Node newNode = new Node(data);
+            if (head == null) {
+                head = newNode;
+                return;
+            }
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+
+        // Method to print the linked list
+        public void printList() {
+            Node current = head;
+            while (current != null) {
+                System.out.print(current.data + " -> ");
+                current = current.next;
+            }
+            System.out.println("null");
+        }
+        
+        // Method to delete a node by value
+        public void deleteByValue(int value) {
+            if (head == null) return;
+
+            // If the head needs to be removed
+            if (head.data == value) {
+                head = head.next;
+                return;
+            } else if(head.data != value) {      
+                
+            }
+
+            Node current = head;
+            while (current.next != null) {
+                if (current.next.data == value) {
+                    current.next = current.next.next; 
+                    return;
+                }
+                current = current.next;
+            }
+        }
+        
+        //Method to swap items in the list
+        public void swapNodes(int index1, int index2) {
+            
+            if (head == null || index1 == index2) return;
+
+            Node current1 = head;
+            Node prev1 = null;
+            Node current2 = head;
+            Node prev2 = null;
+
+          
+            }
+        
+
+}
